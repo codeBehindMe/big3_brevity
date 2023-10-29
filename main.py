f12846e3bfa7903fb2c9bef52e4af3bdf5e6031f
codeBehindMe <aaron.tillekeratne@gmail.com>
2023-10-29 10:06:44 +0000

    added example format and key uppercase

--- main.py
+++ main.py
@@ -11,6 +11,33 @@ import pathlib
 
 mof_limiter = asyncio.Semaphore(1000)  # max open files limiter
 
+EXAMPLE_FORMAT = """{"Monday": {
+    "Session": 7,
+    "Objective": "Strength",
+    "Warm up": [
+      "3 Rounds",
+      "Barbell Complex @ 45/65#",
+      "6x Hand Release Push Ups",
+      "Instep Stretch",
+      "5x Shoulder Dislocates"
+    ],
+    "Training": [
+      "8x Back Squat @ 50% 1RM, then \u2026. 6x Back Squat @ 70% 1RM, then \u2026.",
+      "5 Rounds, Every 90 Seconds:",
+      "4x Back Squat @ 85% 1RM",
+      "8x Bench Press @ 50% 1RM, then .... 6x Bench Press @ 70% 1RM, then \u2026.",
+      "5 Rounds, Every 90 Seconds:",
+      "4x Bench Press @ 85% 1RM",
+      "8x Dead Lift @ 50% 1RM, then \u2026 6x Dead Lift @ 70% 1RM, then \u2026",
+      "5 Rounds, Every 90 Seconds:",
+      "4x Dead Lift @ 85% 1RM",
+      "5 Rounds, Every 90 Seconds",
+      "35% Max Rep Pull Ups",
+      "Foam Roll Legs, Low Back"
+    ],
+    "Comments": "Use your SESSION 1 Strength Assessment results to calculate today\u2019s loading for the Back Squat, Bench Press and Dead Lift, and the reps for the Pull Ups."
+  }}"""
+
 __key_set = False
 
 logging.basicConfig()
@@ -24,17 +51,31 @@ def __set_key():
             openai.api_key = f.readline()
 
 
+def _uppercase_keys(d: dict) -> dict:
+    ud = {}
+    for k, v in d.items():
+        if isinstance(v, dict):
+            v = _uppercase_keys(v)
+        ud[k.upper()] = v
+    return ud
+
+
 async def _summarize_content(content: str):
     logging.info("requesting summary from gpt-3.5.-turbo")
     resp = await openai.ChatCompletion.acreate(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "summarise workout plan text into a JSON"},
+            {
+                "role": "system",
+                "content": f"summarise workout plan text into a JSON; here's an example of the required format style {EXAMPLE_FORMAT}",
+            },
             {"role": "user", "content": content},
         ],
     )
 
-    return json.loads(resp["choices"][0]["message"]["content"])
+    d = json.loads(resp["choices"][0]["message"]["content"])
+
+    return _uppercase_keys(d)
 
 
 async def _week(input, output):
