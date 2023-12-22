474ed0102757dc0e9a3d493f223c98b83a8eebb5
codeBehindMe <aaron.tillekeratne@gmail.com>
2023-12-22 02:10:35 +0000

    added firestore module

---
+++ firestore.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+from google.cloud import firestore as _firestore
+
+
+class Firestore:
+    def __init__(self, database: str) -> None:
+        self.db = _firestore.AsyncClient(database=database)
+
+    async def add_document(
+        self, collection_name: str, document_id: str, document_data: Dict
+    ) -> None:
+        d_ref = self.db.collection(collection_name).document(document_id)
+        return await d_ref.set(document_data=document_data)
